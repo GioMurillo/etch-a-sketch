ba5d6d794f1fb23507eb9eb7edf82f23273bfb58
GioMurillo <giovanni.p.murillo@gmail.com>
2023-08-30 20:47:11 -0700

    Reflexive container
    Move title and container div to HTML from js

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-let page = document.getElementById('page');
-const head1 = document.createElement('h1');
-head1.innerHTML ='Etch-A-Sketch';
-page.appendChild(head1);
-
+let container = document.getElementById('container');
+container.className = 'square';
