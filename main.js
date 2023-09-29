ef9cda0084849aa1c4eaf4082bb84feb74289226
GioMurillo <giovanni.p.murillo@gmail.com>
2023-09-28 21:29:07 -0700

    Begin creating Grid

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-let container = document.getElementById('container');
-container.className = 'square';
+//let container = document.getElementById('container');
+//let numSquares = 16;
+
+//function createGrid(number) {
+  //if(number>100) {
+    //send warning saying too high of number and reduced to 100 max
+    //create grid of 100x100
+      //Generate 10000 squares
+      //insert into container
+  //} else {
+    //create grid of num x num dimension
+  //}
+    
+//}
