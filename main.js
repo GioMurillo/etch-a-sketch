1a5381c6d28935e208344aa48d7336d9e6cfc89f
GioMurillo <giovanni.p.murillo@gmail.com>
2023-10-03 21:56:55 -0700

    Add a button to change row col count

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-let container = document.getElementById('container');
+let container = document.getElementById("container");
 let numSquares = 16;
 
 createGrid(numSquares);
 
+//create a button
+const changeGridSizeButton = document.createElement("button");
+//give button text
+const buttonText = document.createTextNode("Change Row/Column Count");
+changeGridSizeButton.appendChild(buttonText);
+//insert button before the grid
+document.body.insertBefore(changeGridSizeButton, container);
+//add a prompt that will create a new grid
+changeGridSizeButton.addEventListener("click", e => {
+  createGrid(prompt("Enter number under 100", 16))});
+  
 function createGrid(num) {
   if(typeof(num) !== "number") {
     console.log("Entry is not a number. Enter a number between 1 and 100 only.")
@@ -17,7 +28,7 @@ function createGrid(num) {
     //create grid of num x num dimension
     for(i=0; i<numSquares; i++) {
       for(j=0; j<numSquares; j++){
-        const aSquare = document.createElement('div');
+        const aSquare = document.createElement("div");
         aSquare.classList.add("square");
         container.appendChild(aSquare);
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -28,11 +39,11 @@ function createGrid(num) {
 }
 
 //create an array of the squares in the document
-const squares = Array.from(document.querySelectorAll('.square'));
+const squares = Array.from(document.querySelectorAll(".square"));
 //add an event listener for a mouseover on the squares
 squares.forEach(square => square.addEventListener("mouseover", shadeSquare));
 
 //function to change the class of the squares
 function shadeSquare(e) {
-  e.target.classList.add('shaded');
+  e.target.classList.add("shaded");
 }
