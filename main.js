ac24386ef07ee653af0a8aca68f8f03d609ee2e0
GioMurillo <giovanni.p.murillo@gmail.com>
2023-10-04 21:28:58 -0700

    Improve change row column button action

--- main.js
+++ main.js
@@ -8,18 +8,19 @@ const changeGridSizeButton = document.createElement("button");
 //give button text
 const buttonText = document.createTextNode("Change Row/Column Count");
 changeGridSizeButton.appendChild(buttonText);
-//insert button before the grid
+//insert button before the grid container
 document.body.insertBefore(changeGridSizeButton, container);
 //add a prompt that will create a new grid
 changeGridSizeButton.addEventListener("click", e => {
-  createGrid(prompt("Enter number under 100", 16))});
+  container.replaceChildren(); //delete all divs within container
+  createGrid(Number(prompt("Enter number under 100", 16)))});
   
 function createGrid(num) {
   if(typeof(num) !== "number") {
-    console.log("Entry is not a number. Enter a number between 1 and 100 only.")
+    alert("Entry is not a number. Enter a number between 1 and 100 only.")
   } else if(num>100) {
-    console.log("Numbers greater than 100 not accepted. Max grid size created.")
-
+    alert("Numbers greater than 100 not accepted. Max grid size created.")
+    createGrid(100);
     //send warning saying too high of number and reduced to 100 max
     //create grid of 100x100
       //Generate 10000 squares
@@ -31,17 +32,19 @@ function createGrid(num) {
         const aSquare = document.createElement("div");
         aSquare.classList.add("square");
         container.appendChild(aSquare);
+        //this needs to change every time numSquares is changed
         container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
         container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
       }
     }
+    //create an array of the squares in the document
+    const squares = Array.from(document.querySelectorAll(".square"));
+    //add an event listener for a mouseover on the squares
+    squares.forEach(square => square.addEventListener("mouseover", shadeSquare));
   }
 }
 
-//create an array of the squares in the document
-const squares = Array.from(document.querySelectorAll(".square"));
-//add an event listener for a mouseover on the squares
-squares.forEach(square => square.addEventListener("mouseover", shadeSquare));
+
 
 //function to change the class of the squares
 function shadeSquare(e) {
